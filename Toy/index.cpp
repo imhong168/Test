be8c89be1325187c6456bcb1c45bfeffb9269489
imhong168 <seakimhong68@gmail.com>
2024-11-16 00:52:15 +0700

    v3

--- Toy/index.cpp
+++ Toy/index.cpp
@@ -3,8 +3,11 @@ using namespace std;
 int main()
 {
     cout << "hello world" << endl;
-   
+
 
     cout<<"my name is kimhong";
+
+    cout<<"i'm 19 years old"<<endl;
+    cout<<"i studying telecom at NPIC"<<endl;
     return 0;
 }
